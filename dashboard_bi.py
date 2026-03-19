5c9731248fb99b42e48e4b5e4badc447ab5a1c71
TorredaLogistica <logistica.armazenagem.claro@gmail.com>
2026-03-18 22:05:08 -0300

    Update dashboard_bi.py

--- dashboard_bi.py
+++ dashboard_bi.py
@@ -71,7 +71,7 @@ def obter_meta_dinamica(mes, empresas_selecionadas):
 # =============================
 # CARGA DE DADOS (CORRIGIDA PARA .XLSB E GITHUB)
 # =============================
-@st.cache_data
+@st.cache_data(watch_skip_files=[])
 def load_data(path):
     # engine='pyxlsb' é necessário para arquivos .xlsb
     df = pd.read_excel(path, engine='pyxlsb')
